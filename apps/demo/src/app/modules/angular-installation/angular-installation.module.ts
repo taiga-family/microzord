0bd95fe6f5b0e935f4971b094a44c8e4d77724f5
null <taiga-family-bot@yandex.ru>
2024-12-08 20:36:34 +0000

    chore: update @taiga-ui/* deps to v4

--- apps/demo/src/app/modules/angular-installation/angular-installation.module.ts
+++ apps/demo/src/app/modules/angular-installation/angular-installation.module.ts
@@ -1,17 +1,16 @@
+import { TuiLink } from "@taiga-ui/core";
+import { TuiAddonDoc } from "@taiga-ui/addon-doc";
 import {NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
-import {TuiAddonDocModule, tuiGenerateRoutes} from '@taiga-ui/addon-doc';
-import {TuiLinkModule} from '@taiga-ui/core';
-
 import {HostChildWarningModule} from '../../shared/host-child-warning/host-child-warning.module';
 import {AngularInstallationComponent} from './angular-installation.component';
 
 @NgModule({
     imports: [
-        TuiLinkModule,
-        TuiAddonDocModule,
+        TuiLink,
+        ...TuiAddonDoc,
         HostChildWarningModule,
-        RouterModule.forChild(tuiGenerateRoutes(AngularInstallationComponent)),
+        RouterModule,
     ],
     declarations: [AngularInstallationComponent],
     exports: [AngularInstallationComponent],
