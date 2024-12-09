14c7b77c7af7930627a7bc62b219b199c6a048d9
null <taiga-family-bot@yandex.ru>
2024-12-09 09:38:22 +0000

    chore: update @taiga-ui/* deps to v4

--- apps/demo/src/app/getting-started/getting-started.module.ts
+++ apps/demo/src/app/getting-started/getting-started.module.ts
@@ -1,22 +1,20 @@
+import { TuiIslandDirective } from "@taiga-ui/legacy";
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
-import {TuiDocCodeModule, TuiDocPageModule} from '@taiga-ui/addon-doc';
-import {TuiLinkModule, TuiNotificationModule} from '@taiga-ui/core';
-import {TuiIslandModule} from '@taiga-ui/kit';
-
+import { TuiAddonDoc } from '@taiga-ui/addon-doc';
+import { TuiNotification, TuiLink } from '@taiga-ui/core';
 import {HostChildWarningModule} from '../shared/host-child-warning/host-child-warning.module';
 import {GettingStartedComponent} from './getting-started.component';
 
 @NgModule({
     imports: [
         CommonModule,
-        TuiDocPageModule,
-        TuiLinkModule,
-        TuiDocCodeModule,
+        ...TuiAddonDoc,
+        TuiLink,
         RouterModule,
-        TuiNotificationModule,
-        TuiIslandModule,
+        TuiNotification,
+        TuiIslandDirective,
         HostChildWarningModule,
     ],
     declarations: [GettingStartedComponent],
