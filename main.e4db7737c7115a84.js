a28cc56c48727121c02c7e0da6738ac6b2bb4247
mdlufy <zidakbew@gmail.com>
2026-09-18 13:00:58 +0000

    Deploying to gh-pages from @ taiga-family/microzord@8d3ac6b73d595ce23a27e27d9a874f72b6dc5312 🚀

--- main.e4db7737c7115a84.js
+++ main.e4db7737c7115a84.js
@@ -246,7 +246,13 @@ Promise.all(/* import() */[__webpack_require__.e(316), __webpack_require__.e(837
 /******/ 	
 /******/ 	/* webpack/runtime/publicPath */
 /******/ 	(() => {
-/******/ 		__webpack_require__.p = "/";
+/******/ 		var scriptUrl;
+/******/ 		if (typeof import.meta.url === "string") scriptUrl = import.meta.url
+/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
+/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
+/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
+/******/ 		scriptUrl = scriptUrl.replace(/^blob:/, "").replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
+/******/ 		__webpack_require__.p = scriptUrl;
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/consumes */
